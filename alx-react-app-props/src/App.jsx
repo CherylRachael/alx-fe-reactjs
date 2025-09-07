2d6dba9e92b3301044a44c9cd5d716f66e4d214a
Cheryl Rachael <cherylrachael15@gmail.com>
2025-09-07 14:06:25 +0300

    Refactored app to use Context API instead of prop drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,6 @@ import ProfilePage from './components/ProfilePage';
 import UserContext from './UserContext';
 
 function App() {
-  // The data we want to share across components
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
